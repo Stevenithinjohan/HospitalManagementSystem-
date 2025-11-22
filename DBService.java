b47adda943c2e1005627350c5ac4f8c2a2125450
Steve <146315454+Stevenithinjohan@users.noreply.github.com>
2025-11-22 13:06:21 +0530

    Add files via upload

---
+++ DBService.java
@@ -0,0 +1,28 @@
+package com.jts.hms;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBService {
+	public static Connection conn;
+
+	private static Connection createConn() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/training", "root", "password");
+
+		System.out.println("Database connection created successfully.");
+
+		return conn;
+	}
+
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		if (conn == null) {
+			return createConn();
+		}
+
+		return conn;
+	}
+}
+
---
+++ Patient.java
@@ -0,0 +1,5 @@
+package com.jts.hms;
+
+public class Patient {
+
+}
